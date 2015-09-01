ed65e06b8f53f7e2fb93269857300ddbd888d7e9
Jan Sorgalla <jan.sorgalla@dotsunited.de>
2015-09-01 14:08:37 +0200

    Fix __webpack_public_path__

--- assets/webpack-public-path.js
+++ assets/webpack-public-path.js
@@ -1,3 +1,7 @@
-if (!window.__webpack_public_path__) {
-    window.__webpack_public_path__ = document.documentElement.getAttribute('data-theme-url') + '/assets/scripts/';
-}
+__webpack_public_path__ = (function(window, document) {
+    if (!window.__webpack_public_path__) {
+        window.__webpack_public_path__ = document.documentElement.getAttribute('data-theme-url') + '/assets/scripts/';
+    }
+
+    return window.__webpack_public_path__;
+})(window, document);
