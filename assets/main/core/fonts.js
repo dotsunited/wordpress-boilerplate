f0453010ec9d80240c6ae3149f86c772e9ff5b58
Jan Sorgalla <jan.sorgalla@dotsunited.de>
2015-09-18 17:45:08 +0200

    Font loading tweaks

--- assets/main/core/fonts.js
+++ assets/main/core/fonts.js
@@ -13,7 +13,13 @@ if (!html.is('.fonts-loaded')) {
         }
     };
 
-    $.getScript('//ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js');
+    // Don't use $.getScript() as it uses cache: false
+    // https://api.jquery.com/jquery.getscript/#caching-requests
+    $.ajax({
+        dataType: 'script',
+        cache: true,
+        url: '//ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js'
+    });
 
     var date = new Date();
     date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
