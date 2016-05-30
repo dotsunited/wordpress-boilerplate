d140522bb5cff07db7c9f411bc9b266c44ad45d1
Jan Sorgalla <jan.sorgalla@dotsunited.de>
2016-05-30 13:16:19 +0200

    Tweak webpack config formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-var path = require("path");
-var webpack = require("webpack");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // PostCSS plugins
 var cssnext = require('postcss-cssnext');
@@ -9,14 +9,14 @@ var postcssReporter = require('postcss-reporter');
 
 module.exports = {
     entry: {
-        main: ["./assets/webpack-public-path", "./assets/main"],
-        'main-critical': ["./assets/webpack-public-path", "./assets/main/index-critical"]
+        main: ['./assets/webpack-public-path', './assets/main'],
+        'main-critical': ['./assets/webpack-public-path', './assets/main/index-critical']
     },
     output: {
-        path: path.join(__dirname, "web/wp-content/themes/wordpress-boilerplate/assets/scripts"),
-        //publicPath: "/wp-content/themes/wordpress-boilerplate/assets/scripts/",
-        filename: "[name].js", // append ?[hash] to fix entry chunks not updated correctly
-        chunkFilename: "[name].[chunkhash].js"
+        path: path.join(__dirname, 'web/wp-content/themes/wordpress-boilerplate/assets/scripts'),
+        //publicPath: '/wp-content/themes/wordpress-boilerplate/assets/scripts/',
+        filename: '[name].js', // append ?[hash] to fix entry chunks not updated correctly
+        chunkFilename: '[name].[chunkhash].js'
     },
     module: {
         loaders: [
@@ -47,15 +47,30 @@ module.exports = {
                 loaders: ['style', 'css']
             },
 
-            { test: /\.(gif|png|jpe?g|svg)(\?.+)?$/, loader: "file?name=static/[hash].[ext]!image-webpack" },
+            {
+                test: /\.(gif|png|jpe?g|svg)(\?.+)?$/,
+                loaders: ['file?name=static/[hash].[ext]', 'image-webpack']
+            },
 
-            { test: /\.(woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file?name=static/[hash].[ext]&mimetype=application/font-woff" },
-            { test: /\.(ttf|eot)(\?.+)?$/, loader: "file?name=static/[hash].[ext]" },
+            {
+                test: /\.(woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loaders: ['file?name=static/[hash].[ext]&mimetype=application/font-woff']
+            },
+            {
+                test: /\.(ttf|eot)(\?.+)?$/,
+                loaders: ['file?name=static/[hash].[ext]']
+            },
 
-            { test: /\.(swf|xap)$/, loader: "file?name=static/[hash].[ext]" },
+            {
+                test: /\.(swf|xap)$/,
+                loaders: ['file?name=static/[hash].[ext]']
+            },
 
             // required for modernizr, see https://github.com/webpack/webpack/issues/512
-            { test: /modernizr\.js$/, loader: "imports?this=>window!exports?window.Modernizr" }
+            {
+                test: /modernizr\.js$/,
+                loaders: ['imports?this=>window', 'exports?window.Modernizr']
+            }
         ]
     },
     postcss: [
@@ -72,8 +87,8 @@ module.exports = {
             // Automtically detect jQuery and $ as free var in modules
             // and inject the jquery library
             // This is required by many jquery plugins
-            jQuery: "jquery",
-            $: "jquery"
+            jQuery: 'jquery',
+            $: 'jquery'
         }),
         new webpack.DefinePlugin({
             'process.env': {
@@ -94,6 +109,6 @@ module.exports = {
         }),
         new webpack.NoErrorsPlugin(),
 
-        new ExtractTextPlugin("[name].css")
+        new ExtractTextPlugin('[name].css')
     ]
 };
