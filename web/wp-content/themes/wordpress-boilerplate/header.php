5847614893a2fe16c1603dd7e96d8465762ae7f6
Jan Sorgalla <jan.sorgalla@dotsunited.de>
2016-06-09 14:32:09 +0200

    Add automated favicon generation

--- web/wp-content/themes/wordpress-boilerplate/header.php
+++ web/wp-content/themes/wordpress-boilerplate/header.php
@@ -5,6 +5,9 @@
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <?php include ABSPATH.'favicons/favicons.html'; ?>
+
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
