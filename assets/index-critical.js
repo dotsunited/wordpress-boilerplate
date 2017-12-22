1f059b6e7a9e06d9fa85a84b3951255201bc1e8d
Fabian <fabian.bentz@dotsunited.de>
2017-12-22 15:27:12 +0100

    add dotsunited polyfill package

--- assets/index-critical.js
+++ assets/index-critical.js
@@ -1,6 +1,8 @@
-import './rel-preload-as-style-polyfill';
+import polyfill from '@dotsunited/load-css-polyfill';
 import './base/index-critical';
 import './elements/index-critical';
 import './objects/index-critical';
 import './utilities/index-critical';
 import './components/index-critical';
+
+polyfill();
