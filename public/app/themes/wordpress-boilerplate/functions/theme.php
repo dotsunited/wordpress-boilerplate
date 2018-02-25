94b06439f2b2221f8c6f52d2f43301d2da05abcf
Jan Sorgalla <jan.sorgalla@dotsunited.de>
2018-02-25 10:03:19 +0100

    Fix formatting

--- public/app/themes/wordpress-boilerplate/functions/theme.php
+++ public/app/themes/wordpress-boilerplate/functions/theme.php
@@ -36,10 +36,8 @@ function wordpress_boilerplate_pagination()
 
 ?>
     <nav class="pagination" role="navigation">
-        <div
-            class="pagination__prev"><?php next_posts_link(__('<span class="meta-nav">&larr;</span> Older posts', 'wordpress-boilerplate')); ?></div>
-        <div
-            class="pagination__next"><?php previous_posts_link(__('Newer posts <span class="meta-nav">&rarr;</span>', 'wordpress-boilerplate')); ?></div>
+        <div class="pagination__prev"><?php next_posts_link(__('<span class="meta-nav">&larr;</span> Older posts', 'wordpress-boilerplate')); ?></div>
+        <div class="pagination__next"><?php previous_posts_link(__('Newer posts <span class="meta-nav">&rarr;</span>', 'wordpress-boilerplate')); ?></div>
     </nav>
 <?php
 }
