be833907a06504e953f93599d23a168976355b28
Jan Sorgalla <jan.sorgalla@dotsunited.de>
2018-10-05 15:14:11 +0200

    Rename gutenberg function file

--- public/app/themes/wordpress-boilerplate/functions.php
+++ public/app/themes/wordpress-boilerplate/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 include __DIR__ . '/functions/assets.php';
-include __DIR__ . '/functions/blocks.php';
+include __DIR__ . '/functions/gutenberg.php';
 include __DIR__ . '/functions/menu.php';
 include __DIR__ . '/functions/reset.php';
 include __DIR__ . '/functions/theme.php';
