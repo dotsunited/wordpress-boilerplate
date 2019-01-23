53a447dd9e196df1d8a0394c1b405861c5ed3dc9
Fabian <fabian.bentz@dotsunited.de>
2019-01-23 13:55:08 +0100

    update dependencies

---
+++ assets/main/icons/index.js
@@ -0,0 +1,4 @@
+import './index.css';
+
+// Import here to add file to manifest.json
+import './symbol-defs.svg';
--- public/app/themes/wordpress-boilerplate/404.php
+++ public/app/themes/wordpress-boilerplate/404.php
@@ -1,1 +1,19 @@
-<?php
+<?php get_header(); ?>
+	
+	<div class="container lg:flex">
+		<main class="w-auto lg:w-3/4 mr-8">
+			
+			<article id="post-<?php the_ID(); ?>" <?php post_class('mb-8'); ?>>
+				
+				<h1 class="leading-none m-0 mb-4">
+					404
+				</h1>
+				
+			</article>
+		
+		</main>
+		
+		<?php get_sidebar(); ?>
+	</div>
+
+<?php get_footer();
