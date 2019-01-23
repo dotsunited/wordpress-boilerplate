267584cee5eb84d8ab6634f8c18b1d7cbc43851a
Jonas Regner <jonas.regner@dotsunited.de>
2019-01-23 10:42:46 +0100

    optimize wordpress setup script

--- ComposerScripts.php
+++ ComposerScripts.php
@@ -177,8 +177,10 @@ class ComposerScripts
 
         $zip = new \ZipArchive;
         if (true === $zip->open(__DIR__ . '/wordpress.zip')) {
-            $zip->extractTo(__DIR__ . '/public/wp');
+            $zip->extractTo(__DIR__ . '/public/wp', 'wordpress');
             $zip->close();
         }
+
+        unlink(__DIR__ . '/wordpress.zip');
     }
 }
