87df15868ea67b3b82a077331d5bf275093270b6
Jonas Regner <jonas.regner@dotsunited.de>
2019-10-24 10:21:06 +0200

    fix rename

--- ComposerScripts.php
+++ ComposerScripts.php
@@ -52,7 +52,7 @@ class ComposerScripts
 
         // --- Copy/rename files & dirs
 
-        rename(__DIR__ . '/public/wp-content/mu-plugins/wordpress-boilerplate/wordpress-boilerplate.php', __DIR__ . '/public/app/mu-plugins/wordpress-boilerplate/' . $projectIdentifier . '.php');
+        rename(__DIR__ . '/public/wp-content/mu-plugins/wordpress-boilerplate/wordpress-boilerplate.php', __DIR__ . '/public/wp-content/mu-plugins/wordpress-boilerplate/' . $projectIdentifier . '.php');
         rename(__DIR__ . '/public/wp-content/mu-plugins/wordpress-boilerplate', __DIR__ . '/public/app/mu-plugins/' . $projectIdentifier);
         rename(__DIR__ . '/public/wp-content/themes/wordpress-boilerplate', __DIR__ . '/public/app/themes/' . $projectIdentifier);
 
