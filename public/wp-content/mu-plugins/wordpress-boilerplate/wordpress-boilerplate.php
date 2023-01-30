d21b93fdf66b83372b7147d52c5addc8e11a46f3
Fabian Brendli <fabian.brendli@dotsunited.de>
2023-01-30 14:03:54 +0100

    release v12.0.1

--- public/wp-content/mu-plugins/wordpress-boilerplate/wordpress-boilerplate.php
+++ public/wp-content/mu-plugins/wordpress-boilerplate/wordpress-boilerplate.php
@@ -3,7 +3,7 @@
  * Plugin Name: Wordpress Boilerplate
  * Description: The main Wordpress Boilerplate plugin.
  * License: MIT
- * Version: 12.0.0
+ * Version: 12.0.1
  * Author: Dots United GmbH
  * Author URI: https://dotsunited.de
  */
