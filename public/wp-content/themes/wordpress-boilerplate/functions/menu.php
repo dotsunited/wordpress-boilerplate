1a66fb25a6227432f0a1ef390f9c9b13e99dacb9
Fabian Brendli <fabian.brendli@dotsunited.de>
2023-11-29 13:33:23 +0100

    fix: use correct screen reader class

--- public/wp-content/themes/wordpress-boilerplate/functions/menu.php
+++ public/wp-content/themes/wordpress-boilerplate/functions/menu.php
@@ -139,7 +139,7 @@ class wordpress_boilerplate_off_canvas_menu_walker extends Walker_Nav_Menu
 		
 		$id = 'off-canvas-sub-menu-' . self::$idCounter++;
 		
-		$output = implode('<ul', $parts) . '<button class="js-off-canvas-menu-submenu-control off-canvas-menu__submenu-control" data-ctrly="' . $id . '"><svg class="icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg><span class="visually-hidden">' . _x('Untermenü öffnen', 'button', 'wordpress-boilerplate') . '</span></button><ul id="' . $id . '"' . $last;
+		$output = implode('<ul', $parts) . '<button class="js-off-canvas-menu-submenu-control off-canvas-menu__submenu-control" data-ctrly="' . $id . '"><svg class="icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg><span class="sr-only">' . _x('Untermenü öffnen', 'button', 'wordpress-boilerplate') . '</span></button><ul id="' . $id . '"' . $last;
 	}
 }
 
