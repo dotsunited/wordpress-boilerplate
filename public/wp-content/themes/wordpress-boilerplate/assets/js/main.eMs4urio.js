73d70d8e5233d29d20ece88b609f0dbc4bbe6f12
Fabian Brendli <fabian.brendli@dotsunited.de>
2023-12-02 00:41:32 +0100

    feat: use bun instead of node

--- public/wp-content/themes/wordpress-boilerplate/assets/js/main.eMs4urio.js
+++ public/wp-content/themes/wordpress-boilerplate/assets/js/main.eMs4urio.js
@@ -1,4 +1,4 @@
-import{_}from"./vendor.LRFSOuOa.js";_(()=>import("./off-canvas-menu.blAeFtJJ.js"),__vite__mapDeps([0,1,2]),import.meta.url).then();
+import{_}from"./vendor.AE9WDIha.js";_(()=>import("./off-canvas-menu.blAeFtJJ.js"),__vite__mapDeps([0,1,2]),import.meta.url).then();
 function __vite__mapDeps(indexes) {
   if (!__vite__mapDeps.viteFileDeps) {
     __vite__mapDeps.viteFileDeps = ["./off-canvas-menu.blAeFtJJ.js","./ctrly.u0PsaQiZ.js","../css/off-canvas-menu.JUOuksh-.css"]
