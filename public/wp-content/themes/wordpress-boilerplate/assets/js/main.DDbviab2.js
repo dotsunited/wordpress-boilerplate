99ada1690cf10f0e3ad0dac49087dbeb9ae11b58
Fabian Brendli <fabian.brendli@dotsunited.de>
2024-03-01 11:29:08 +0100

    chore: update dependencies

---
+++ public/wp-content/themes/wordpress-boilerplate/assets/js/main.DDbviab2.js
@@ -0,0 +1,7 @@
+import{_}from"./vendor.BLotvweF.js";_(()=>import("./off-canvas-menu.DDdqsju5.js"),__vite__mapDeps([0,1,2]),import.meta.url).then();
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./off-canvas-menu.DDdqsju5.js","./ctrly.BjJxFZm9.js","../css/off-canvas-menu.lQ66SyH4.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
