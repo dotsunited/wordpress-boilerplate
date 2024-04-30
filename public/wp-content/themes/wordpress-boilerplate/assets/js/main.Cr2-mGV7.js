a4a088ac0066a3d6f2750aadbf758d3725325d18
Fabian Brendli <fabian.brendli@dotsunited.de>
2024-04-30 12:35:21 +0200

    chore: remove & ignore built assets

--- public/wp-content/themes/wordpress-boilerplate/assets/js/main.Cr2-mGV7.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["./off-canvas-menu.DDdqsju5.js","./ctrly.BjJxFZm9.js","../css/off-canvas-menu.lQ66SyH4.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{_}from"./vendor.DyDGoheb.js";_(()=>import("./off-canvas-menu.DDdqsju5.js"),__vite__mapDeps([0,1,2]),import.meta.url).then();
