de51e5db6823edad2845df236cf7ef02ee1f4a8b
Fabian Brendli <fabian.brendli@dotsunited.de>
2024-05-21 16:36:01 +0200

    chore: update screen reader text

--- public/wp-content/themes/wordpress-boilerplate/template-parts/components/header/desktop.php
+++ public/wp-content/themes/wordpress-boilerplate/template-parts/components/header/desktop.php
@@ -1,6 +1,6 @@
 <div class="container py-2 hidden md:flex flex-row flex-wrap items-center justify-between">
-	<a href="<?= esc_attr(get_home_url()); ?>" class="logo-large items-center flex-shrink-0 text-white mr-6" title="<?= _x('Click here to go back to frontpage', 'logo', 'wordpress-boilerplate'); ?>">
-		<span class="screen-reader-text"><?= _x('Click here to go back to frontpage', 'logo', 'wordpress-boilerplate'); ?></span>
+	<a href="<?= esc_attr(get_home_url()); ?>" class="logo-large items-center flex-shrink-0 text-white mr-6" title="<?= _x('Back to frontpage', 'logo', 'wordpress-boilerplate'); ?>">
+		<span class="screen-reader-text"><?= _x('Back to frontpage', 'logo', 'wordpress-boilerplate'); ?></span>
 		<?= wordpress_boilerplate_asset_embed('/vendor/images/logo-dotsunited.svg'); ?>
 	</a>
 	
