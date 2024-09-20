25dac7d302e2a44c6aded5a095904851d28a5024
Fabian Brendli <fabian.brendli@dotsunited.de>
2024-09-20 17:20:36 +0200

    chore: adjust order of imports

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss';
-import plugin from 'tailwindcss/plugin';
-import defaultTheme from 'tailwindcss/defaultTheme';
 import type { CSSRuleObject } from 'tailwindcss/types/config';
+import defaultTheme from 'tailwindcss/defaultTheme';
+import plugin from 'tailwindcss/plugin';
 
 export default {
     content: [
