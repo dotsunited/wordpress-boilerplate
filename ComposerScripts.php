f32d3d6db80a9bd210b44c31ae21b059c6a24258
Fabian Brendli <fabian.brendli@dotsunited.de>
2024-09-24 17:15:28 +0200

    refactor: use ftp-deploy for deployments to FTP

--- ComposerScripts.php
+++ ComposerScripts.php
@@ -38,8 +38,6 @@ class ComposerScripts
         // --- Replace in files & dirs
 
         self::replace(__DIR__ . '/.gitignore', $projectName, $projectIdentifier);
-        self::replace(__DIR__ . '/.git-ftp-include', $projectName, $projectIdentifier);
-        self::replace(__DIR__ . '/.eslintrc.cjs', $projectName, $projectIdentifier);
         self::replace(__DIR__ . '/package.json', $projectName, $projectIdentifier);
         self::replace(__DIR__ . '/README.md.template', $projectName, $projectIdentifier);
         self::replace(__DIR__ . '/tailwind.config.ts', $projectName, $projectIdentifier);
